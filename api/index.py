ecac8a0251ffe7b4ffc1636a054e1dec6d04f976
רואה ואינו נראה <matanyredate@gmail.com>
2025-12-18 19:13:18 +0200

    Update and rename index.py to index

--- api/index.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, Response
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def main():
-    return Response("id_list_message=t-החיבור עובד", mimetype="text/plain")
