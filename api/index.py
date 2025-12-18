96054188711536065716134bdf281b8def58b7b7
רואה ואינו נראה <matanyredate@gmail.com>
2025-12-18 18:21:19 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,2 +1,8 @@
-def handler(request):
-    return "id_list_message=t-החיבור עובד"
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    return Response("id_list_message=t-החיבור עובד", mimetype="text/plain")
+
